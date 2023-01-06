4932141ea4d75b90ae8ad1f03a4c895f885b6e8d
waiyan93 <waiyanhtet.dev@gmail.com>
2023-01-07 02:30:40 +0700

    Added invalidation to push

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,7 +6,7 @@ const packageJson = require('../package.json');
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contentHash].js'
+        filename: '[name].[contenthash].js'
     },
     plugins: [
         new ModuleFederationPlugin({
